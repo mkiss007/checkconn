4673be504eccb746f478f54d44138738afe772db
Miklós Kiss <54062368+mkiss007@users.noreply.github.com>
2020-08-01 13:21:55 +0200

    Update application.py

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello Chernobyl"
+    return "Hello Chernobyl!"
 	
 @app.route('/mysqldev', methods=['GET'])
 def mysql():
