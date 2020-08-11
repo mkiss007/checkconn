1b7d1f9abedfa4db022367305b7720db7b1d3ade
Miklós Kiss <54062368+mkiss007@users.noreply.github.com>
2020-08-11 12:11:02 +0200

    Update application.py

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello Chernobyl!"
+    return "Hello Chernobyl! (2020.08.11)"
 	
 @app.route('/mysqldev', methods=['GET'])
 def mysql():
@@ -60,7 +60,7 @@ def check(address,port):
    return(my_return)	
 
 	
-@app.route('/wheather',methods=['GET'])
+@app.route('/weather',methods=['GET'])
 def wheather():
     url = "http://api.openweathermap.org/data/2.5/weather?id=3046526&APPID=8f6abb37f67a3cb3dcef2f6023a92641&units=metric&mode=html"
     response = requests.get(url)
